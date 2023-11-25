039cef816ccd9fd89b90f169eded28d2a0c2188e
Chengyu <chengyuz0921@gmail.com>
2023-11-25 17:07:38 -0500

    Create CreateButton.tsx

--- website/FamilyPlus/src/main.tsx
+++ website/FamilyPlus/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+import { DAppProvider } from "@usedapp/core";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* 
+       Wrap our app in the provider, config is required, 
+        but can be left as an empty object: 
+    */}
+    <DAppProvider config={{}}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
