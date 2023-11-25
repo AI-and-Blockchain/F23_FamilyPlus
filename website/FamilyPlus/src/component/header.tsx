ebcfd6dc6e3cbb57300a31f21c5e43829ce27c0d
Chengyu <chengyuz0921@gmail.com>
2023-11-25 16:37:02 -0500

    Create a new header file

---
+++ website/FamilyPlus/src/component/header.tsx
@@ -0,0 +1,33 @@
+import Logo from '../image/logo.png';
+import { Link } from 'react-router-dom';
+
+/* 
+creates the header for the website including the logo and the buttons to navigate
+*/
+export default function Headers() {
+    return (
+        <header className="bg-white-300">
+            <div className="container mx-auto flex items-center justify-between p-4">
+                <div className="flex items-center">
+                    <Link to="/">
+                        <img src={Logo} alt="Logo" className="w-12 h-12" />
+                    </Link>
+
+                    <h1 className="text-xl font-bold text-black ml-2"></h1>
+                </div>
+                <nav>
+                    <ul className="flex space-x-3">
+                        <li>
+                            <Link
+                                to="/contact-us"
+                                className="font-bold px-4 py-2 rounded-lg hover:bg-red-600 transition-colors duration-300"
+                            >Contact Us</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+
+        </header>
+    );
+}
