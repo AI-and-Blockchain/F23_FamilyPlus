39eab818598c97f602dde684b7a543e8a9f3683d
Ethan Gertler <edgchess@gmail.com>
2023-11-27 23:06:33 -0500

    Use window.crypto.subtle instead on node:crypto

--- Crypto/crypto.ts
+++ Crypto/crypto.ts
@@ -1,7 +1,12 @@
-import { randomBytes, createCipheriv, createDecipheriv, Cipher, Decipher } from 'node:crypto';
 import { Buffer } from 'node:buffer';
 import * as nacl from 'tweetnacl';
 import * as naclUtil from 'tweetnacl-util';
+
+const SYMMETRIC_ALGORITHM_NAME: string = 'AES-CBC';
+const SYMMETRIC_KEY_LENGTH: number = 256;
+const SYMMETRIC_ALGORITHM: AesKeyGenParams = {name: SYMMETRIC_ALGORITHM_NAME, length: SYMMETRIC_KEY_LENGTH};
+const ASYMMETRIC_ALGORITHM: string = 'x25519-xsalsa20-poly1305';
+const IV_LENGTH: number = 16;
 
 export type AsymmetricEncryptedData = {
     nonce: Buffer;
@@ -13,6 +18,12 @@ export type EncryptedData = {
     encrypted_key: AsymmetricEncryptedData;
     encrypted_document: Buffer;
 };
+
+function randomBytes(amount: number): Buffer {
+    let data = Buffer.alloc(amount);
+    window.crypto.getRandomValues(data);
+    return data;
+}
 
 function asymmetric_encrypt(publicKey: Buffer, data: Buffer): AsymmetricEncryptedData {
     const nonce: Buffer = randomBytes(nacl.box.nonceLength);
@@ -38,7 +49,7 @@ async function asymmetric_decrypt(address: string, encryptedData: AsymmetricEncr
 
 function metamaskify_encrypted_data(encryptedData: AsymmetricEncryptedData): string {
     const data = {
-        version: 'x25519-xsalsa20-poly1305',
+        version: ASYMMETRIC_ALGORITHM,
         nonce: naclUtil.encodeBase64(encryptedData.nonce),
         ephemPublicKey: naclUtil.encodeBase64(encryptedData.ephemeralPublicKey),
         ciphertext: naclUtil.encodeBase64(encryptedData.ciphertext),
@@ -58,17 +69,14 @@ function decode_asymmetric_encrypted_data(encodedData: Buffer): AsymmetricEncryp
     };
 }
 
-function encrypt_document(publicKey: Buffer, document_data: Buffer): EncryptedData {
-    const IV_LENGTH = 16;
-    const algorithm: string = 'aes-256-cbc';
-    const symmetric_key: Buffer = randomBytes(32);
-    const encrypted_key: AsymmetricEncryptedData = asymmetric_encrypt(publicKey, symmetric_key);
+async function encrypt_document(publicKey: Buffer, document_data: Buffer): Promise<EncryptedData> {
+    const symmetric_key: CryptoKey = await window.crypto.subtle.generateKey(SYMMETRIC_ALGORITHM, true, ["encrypt"]);
+    const symmetric_key_data: Buffer = await window.crypto.subtle.exportKey("raw", symmetric_key).then(Buffer.from);
+    const encrypted_key: AsymmetricEncryptedData = asymmetric_encrypt(publicKey, symmetric_key_data);
     const iv: Buffer = randomBytes(IV_LENGTH);
-    let cipher: Cipher = createCipheriv(algorithm, symmetric_key, iv);
 
-    let encrypted: Buffer = cipher.update(document_data);
-	const buffer: Buffer = cipher.final();
-	encrypted = Buffer.concat([iv, encrypted, buffer], IV_LENGTH + encrypted.length + buffer.length);
+    let encrypted: Buffer = await window.crypto.subtle.encrypt({name: SYMMETRIC_ALGORITHM_NAME, iv:iv}, symmetric_key, document_data).then(Buffer.from);
+	encrypted = Buffer.concat([iv, encrypted], IV_LENGTH + encrypted.length);
 
     return {
         encrypted_key: encrypted_key,
@@ -78,13 +86,10 @@ function encrypt_document(publicKey: Buffer, document_data: Buffer): EncryptedDa
 
 async function decrypt_document(address: string, encryptedData: EncryptedData): Promise<Buffer> {
     // Returns a decrypted Buffer of the original document_data
-    const IV_LENGTH = 16;
-    const algorithm: string = 'aes-256-cbc';
-    const symmetric_key: Buffer = await asymmetric_decrypt(address, encryptedData.encrypted_key);
+    const symmetric_key_data: Buffer = await asymmetric_decrypt(address, encryptedData.encrypted_key);
+    const symmetric_key: CryptoKey = await window.crypto.subtle.importKey("raw", symmetric_key_data, SYMMETRIC_ALGORITHM, false, ["decrypt"]);
     const iv: Buffer = encryptedData.encrypted_document.subarray(0, IV_LENGTH);
-    encryptedData.encrypted_document = encryptedData.encrypted_document.subarray(IV_LENGTH);
-    let decipher: Decipher = createDecipheriv(algorithm, symmetric_key, iv);
-    const decrypted: Buffer = decipher.update(encryptedData.encrypted_document);
-	const buffer: Buffer = decipher.final();
-    return Buffer.concat([decrypted, buffer], decrypted.length + buffer.length);
+    let encrypted_document = encryptedData.encrypted_document.subarray(IV_LENGTH);
+    const decrypted: Buffer = await window.crypto.subtle.decrypt({name: SYMMETRIC_ALGORITHM_NAME, iv:iv}, symmetric_key, encrypted_document).then(Buffer.from);
+    return decrypted
 }
