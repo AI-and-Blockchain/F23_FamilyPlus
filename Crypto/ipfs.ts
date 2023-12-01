2e33fab0402e7481166775a60c7f92efec3be420
Ethan Gertler <edgchess@gmail.com>
2023-11-30 21:59:25 -0500

    Changed uploadFile to take a Buffer instead of a FilePath

--- Crypto/ipfs.ts
+++ Crypto/ipfs.ts
@@ -1,6 +1,5 @@
 import { createHelia } from 'helia';
 import { unixfs } from '@helia/unixfs';
-import * as fs from 'fs';
 import { CID } from 'multiformats/cid';
 
 // Connect to IPFS
@@ -8,8 +7,7 @@ const helia = await createHelia();
 const helia_fs = unixfs(helia);
 
 // Upload File
-async function uploadFile(filePath: string) : Promise<Buffer> {
-    const fileContent: Buffer = fs.readFileSync(filePath);
+async function uploadFile(fileContent: Buffer) : Promise<Buffer> {
     const result: CID = await helia_fs.addBytes(fileContent);
     return Buffer.from(result.toV1().bytes);
 }
